e9ac5e34ff6fd5d49fa80f1f15816d79b98423f8
Princegautam-code <Princeiit46@gmail.com>
2026-08-19 20:53:15 +0530

    Add day 10 program 1

---
+++ day-10-program-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    double a, b, c;
+
+    printf("Enter three side lengths: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (a <= 0 || b <= 0 || c <= 0 ||
+        a + b <= c || a + c <= b || b + c <= a) {
+        printf("Invalid triangle\n");
+    } else if (a == b && b == c) {
+        printf("Equilateral triangle\n");
+    } else if (a == b || a == c || b == c) {
+        printf("Isosceles triangle\n");
+    } else {
+        printf("Scalene triangle\n");
+    }
+
+    return 0;
+}
