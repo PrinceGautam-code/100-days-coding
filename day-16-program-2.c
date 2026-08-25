22a6bd7f28060436c8bcd365060d36932f76f4ba
Princegautam-code <Princeiit46@gmail.com>
2026-08-25 22:59:29 +0530

    Add day-16-program-2.c

---
+++ day-16-program-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, originalNum, remainder, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0)
+    {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (originalNum == reverse)
+    {
+        printf("%d is a palindrome number.\n", originalNum);
+    }
+    else
+    {
+        printf("%d is not a palindrome number.\n", originalNum);
+    }
+
+    return 0;
+}
