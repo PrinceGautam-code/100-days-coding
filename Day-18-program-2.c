c265ab821562b96a50ad56ed73a2dbbc8ef9ad6f
Princegautam-code <Princeiit46@gmail.com>
2026-08-30 02:50:01 +0530

    Add day-18 program-2: Find HCF of two numbers

---
+++ Day-18-program-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, i, hcf;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    hcf = 1;
+
+    for (i = 1; i <= num1 && i <= num2; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            hcf = i;
+        }
+    }
+
+    printf("HCF (GCD) = %d\n", hcf);
+
+    return 0;
+}
