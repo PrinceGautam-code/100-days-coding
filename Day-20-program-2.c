1f123316899e4723d6e039362f142a1b05b010d4
Princegautam-code <Princeiit46@gmail.com>
2026-08-30 02:23:18 +0530

    Add day-20 program-2: Find 1's complement of a binary number

---
+++ Day-20-program-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    char binary[100];
+
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    printf("1's Complement: ");
+
+    for (int i = 0; binary[i] != '\0'; i++)
+    {
+        if (binary[i] == '0')
+            printf("1");
+        else if (binary[i] == '1')
+            printf("0");
+        else
+        {
+            printf("\nInvalid binary number!\n");
+            return 1;
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
