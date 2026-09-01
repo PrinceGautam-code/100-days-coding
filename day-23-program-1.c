97ea69614e39bf246edc7a401ac3bf62a032c702
Princegautam-code <Princeiit46@gmail.com>
2026-09-01 14:30:54 +0530

    Add Day 23 Program 1 - Sum of Series

---
+++ day-23-program-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    double sum = 0.0;
+    int numerator = 2, denominator = 3;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum = sum + (double)numerator / denominator;
+        numerator += 2;
+        denominator += 4;
+    }
+
+    printf("Sum of the series = %.2lf\n", sum);
+
+    return 0;
+}
