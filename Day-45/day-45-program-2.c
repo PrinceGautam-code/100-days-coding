6b33aceeb0624f17994e69a49907c72d16dfe049
Princegautam-code <Princeiit46@gmail.com>
2026-09-23 01:35:56 +0530

    Add Day 45 folder

---
+++ Day-45/day-45-program-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Toggle case of each character
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+
+        i++;
+    }
+
+    printf("String after toggling case: %s", str);
+
+    return 0;
+}
